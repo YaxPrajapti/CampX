46b6d95e5e4bfbc10a1ba6b85125b83347ddf82b
Yax Prajapati <92200237+YaxPrajapti@users.noreply.github.com>
2024-06-21 23:48:23 +0530

    Added functionality to upload images to cloudinary. Link to store on db is yet to done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,17 +5,25 @@ const Review = require('../models/review');
 const { validateCampground, validateReview } = require('../utils/validations.js');
 const { isLoggedIn, isAuthorised } = require('../middleware.js');
 const campgroundController = require('../controller/campgroundController.js');
+const multer = require('multer');
+const { storage } = require('../utils/cloudinary.js');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgroundController.getCampgrounds))
-    .post(isLoggedIn, validateCampground, catchAsync(campgroundController.postNewCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgroundController.postNewCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("It worked");
+    })
+
+router.get('/new', isLoggedIn, campgroundController.getNewCampground);
 
 router.route('/:id')
     .get(catchAsync(campgroundController.getCampgroundFromId))
     .put(isLoggedIn, isAuthorised, validateCampground, catchAsync(campgroundController.putCampgroundEdit))
     .delete(isAuthorised, catchAsync(campgroundController.deleteCampground));
 
-router.get('/new', isLoggedIn, campgroundController.getNewCampground);
 
 router.get('/:id/edit', isLoggedIn, isAuthorised, catchAsync(campgroundController.getCampgroundEdit));
 
